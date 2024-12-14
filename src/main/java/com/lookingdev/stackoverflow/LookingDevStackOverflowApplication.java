90d906f5034da05d295d6a03d513d3bb06767c1d
freshhuk <freshhuk13@gmail.com>
2024-12-14 23:38:15 +0200

    Initial commit

---
+++ src/main/java/com/lookingdev/stackoverflow/LookingDevStackOverflowApplication.java
@@ -0,0 +1,13 @@
+package com.lookingdev.stackoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookingDevStackOverflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookingDevStackOverflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lookingdev/stackoverflow/LookingDevStackOverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lookingdev.stackoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookingDevStackOverflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
